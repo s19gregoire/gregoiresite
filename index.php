3e42c27a6ec2a81b657e9b6b4232a3ac22a59c85
Gregoire Neve <gregoire.neve@gmail.com>
2021-03-13 12:04:44 +0000

    upload of the site

--- index.php
+++ index.php
@@ -4,10 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          <link rel="stylesheet" href="css/index.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Gneve.eu shareyour video online</title>
 </head>
 <body>
+<div class="menu">
 
+    <ul>
+    <li><a href="about.php">A propos</a></li>
+    <li><a href="abonnement.php">abonnement</a></li>
+    <li><a href="api/">Documentation pour dévellopeurs</a></li>
+    </ul>
+</div>
+<div class="container">
+</div>
 </body>
 </html>
